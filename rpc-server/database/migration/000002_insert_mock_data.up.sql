0383a9e37246c6d861c942635282cb46a7c85158
jh-chee <hardfastfurioushff@gmail.com>
2023-06-10 23:18:40 +0800

    fix: fix next cursor value, remove mock data (#4)

--- rpc-server/database/migration/000002_insert_mock_data.up.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO messages (chat_room, sender, body)
-VALUES
-    ('Sender1:Receiver1', 'Sender1', 'SampleBody'),
-    ('Sender2:Receiver2', 'Sender2', 'SampleBody'),
-    ('Sender3:Receiver3', 'Sender3', 'SampleBody'),
-    ('Sender4:Receiver4', 'Sender4', 'SampleBody'),
-    ('Sender5:Receiver5', 'Sender5', 'SampleBody');
